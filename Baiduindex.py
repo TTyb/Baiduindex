d58bb5c39687a7fbf84e0e6d49ec1edfe449c2ee
TTyb <420439007@qq.com>
2016-11-08 18:28:54 +0800

    ttyb

--- Baiduindex.py
+++ Baiduindex.py
@@ -10,6 +10,8 @@
 
 import time
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from PIL import Image
 
 
 # 打开浏览器
@@ -106,9 +108,9 @@ def openbrowser():
             select = input("请观察浏览器网站是否已经登陆(y/n)：")
 
 
-def getindex(keyword):
+def getindex(keyword="希拉里"):
     openbrowser()
-    time.sleep(5)
+    time.sleep(2)
 
     # 这里开始进入百度指数
     # 要不这里就不要关闭了，新打开一个窗口
@@ -136,12 +138,49 @@ def getindex(keyword):
     browser.find_element_by_xpath('//a[@rel="7"]').click()
     # 最大化窗口
     browser.maximize_window()
-
+    # 太快了
+    time.sleep(2)
     # 滑动思路：http://blog.sina.com.cn/s/blog_620987bf0102v2r8.html
     # 滑动思路：http://blog.csdn.net/zhouxuan623/article/details/39338511
     # 向上移动鼠标80个像素，水平方向不同
     # ActionChains(browser).move_by_offset(0,-80).perform()
+    # <div id="trend" class="R_paper" style="height:480px;_background-color:#fff;"><svg height="460" version="1.1" width="954" xmlns="http://www.w3.org/2000/svg" style="overflow: hidden; position: relative; left: -0.5px;">
+    # <rect x="20" y="130" width="914" height="207.66666666666666" r="0" rx="0" ry="0" fill="#ff0000" stroke="none" opacity="0" style="-webkit-tap-highlight-color: rgba(0, 0, 0, 0); opacity: 0;"></rect>
+    # xoyelement = browser.find_element_by_xpath('//rect[@stroke="none"]')
+    xoyelement = browser.find_elements_by_css_selector("#trend rect")
+    num = 0
+    # 常用js:http://www.cnblogs.com/hjhsysu/p/5735339.html
+    for item in xoyelement:
+        try:
+            # webdriver.ActionChains(driver).move_to_element().click().perform()
+            # 只有移动位置xoyelement[2]是准确的
+            ActionChains(browser).move_to_element(item).perform()
+            time.sleep(2)
+            # <div class="imgtxt" style="margin-left:-117px;"></div>
+            imgelement = browser.find_element_by_xpath('//div[@id="viewbox"]')
+            # 找到图片坐标
+            locations = imgelement.location
+            print(locations)
+            # 找到图片大小
+            sizes = imgelement.size
+            print(sizes)
+            # 构造位置
+            rangle = (int(locations['x']), int(locations['y']), int(locations['x'] + sizes['width']),
+                      int(locations['y'] + sizes['height']))
+            # 截取当前浏览器
+            path = "../baidu/" + str(num)
+            browser.save_screenshot(str(path) + ".png")
+            # 打开截图切割
+            img = Image.open(str(path) + ".png")
+            jpg = img.crop(rangle)
+            jpg.save(str(path) + ".jpg")
+            num = num + 1
+            input(22222)
+        except:
+            print(num)
+
 
 
 if __name__ == "__main__":
+    getindex(keyword="希拉里")
     pass
